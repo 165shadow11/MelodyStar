682636c3691a70e000956a45fc53d4b333f44f10
Matthew G <matthewgrant55@gmail.com>
2019-10-28 19:49:14 -0400

    Made note names appear in lanes

--- Assets/Scripts/Songs/Gameplay/SongSetup.cs
+++ Assets/Scripts/Songs/Gameplay/SongSetup.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 using CustomInput;
 using Songs.Model;
 using UnityEngine;
-
+using UnityEngine.UI;
 namespace Songs.Gameplay {
 	public class SongSetup : MonoBehaviour {
 
@@ -21,7 +21,7 @@ namespace Songs.Gameplay {
 
 		public List<Lane> setupLanes() {
 			List<Lane> lanes = new List<Lane>();
-
+			
 			//coooooordinate transforms. but undisciplined.
 			float cameraHeight = 10f;
 			float lanePrefabWidth = lanePrefab.GetComponent<SpriteRenderer>().size.x;
@@ -36,7 +36,32 @@ namespace Songs.Gameplay {
 				lane.GetComponent<SpriteRenderer>().size = new Vector2(laneWidth, 10);
 				lanes.Add(lane.GetComponent<Lane>());
 			}
+			string[] notes = {"C","C#","D","D#","E","F","F#","G","G#","A","A#","B"};
+			int index  = 0;
+			for (int j=InputSettings.middleC; j<InputSettings.keys.Length;j++){
+				Lane selectedLane = lanes[j];
+				Text noteText = selectedLane.GetComponentInChildren<Text>();
+				noteText.fontSize = 200;
+				noteText.text = notes[index];
+				index+=1;
+			
+			if (InputSettings.middleC>0){
+				int count = 0;
+				for (int k=InputSettings.middleC-1;k>=0;k--){
+					Lane newLane = lanes[k];
+					Text noteText2 = newLane.GetComponentInChildren<Text>();
+					noteText2.fontSize = 100;
+					noteText2.text = notes[(notes.Length-1)-count];
+					count+=1;
+				}	
+			}
+	
 
+			}
+			/* Lane firstLane = lanes[0];
+			Text firstText = firstLane.GetComponentInChildren<Text>();
+			firstText.text = "test";
+		 */
 			return lanes;
 		}
 	}
